d62ee6c81a4e7427b2a4748cfb53d26b41eb7ee9
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-08-08 17:35:18 +0530

    Done: BinanceClient Initial Implementation

---
+++ BinanceClient.py
@@ -0,0 +1,68 @@
+import websocket
+from websocket import WebSocketApp
+from websocket._abnf import ABNF
+import json
+
+def on_message(ws, message):
+    message = json.loads(message)
+    if not 's' in message.keys():
+        print(message)
+        return
+    time,market,price = message['E'],message['s'],message['p']
+    # print(f"{time} {market}  {price}")
+        
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws, close_status_code, close_msg):
+    subscribe = {
+        "method": "UNSUBSCRIBE",
+        "params":
+        [
+        "btcusdt@aggTrade",
+        "ethusdt@aggTrade",
+        "maticusdt@aggTrade"
+        ],
+        "id": 1
+        }
+    payload = json.dumps(subscribe)
+    ws.send(payload)
+    print("### closed ###")
+
+def on_ping(ws,msg):
+    print(msg)
+    ws.send(msg,ABNF.OPCODE_PONG)
+
+def on_open(ws):
+    print("Opened connection")
+    subscribe = {
+        "method": "SUBSCRIBE",
+        "params":
+        [
+        "btcusdt@aggTrade",
+        "ethusdt@aggTrade",
+        "maticusdt@aggTrade"
+        ],
+        "id": 1
+        }
+    payload = json.dumps(subscribe)
+    ws.send(payload)
+
+    
+   
+    
+class Binance(WebSocketApp):
+    
+    def __init__(self):
+        super().__init__("wss://stream.binance.com:9443/ws",
+                            on_open=on_open,
+                            on_ping = on_ping,
+                            on_message=on_message,
+                            on_error=on_error,
+                            on_close=on_close)
+        super().run_forever(ping_interval=10)
+
+if __name__ == "__main__":
+    # websocket.enableTrace(True)
+    ws = Binance()
