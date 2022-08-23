d1d70a60e2237d72ec4d005478c6b64e08c258af
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-08-23 12:39:35 +0530

    Changed flow for adding prices, now it calculates the average for ts directly instead of adding it to a list

--- Price_store.py
+++ Price_store.py
@@ -1,35 +1,59 @@
 from threading import Lock
 import json
+from datetime import datetime
+
 
 class Price_store:
-    
+
     def __init__(self):
         self.symbols = {
-             "btcusdt":"BTC/USD",
-            "ethusdt":"ETH/USD",
-            "maticusdt":"MATIC/USD",
-            "BTC/USD":"BTC/USD",
-            "ETH/USD":"ETH/USD",
-            "MATIC/USD":"MATIC/USD",
-            "market.btcusdt.trade.detail":"BTC/USD",
-            "market.maticusdt.trade.detail":"ETH/USD",
-            "market.ethusdt.trade.detail":"MATIC/USD"
+            "BTCUSDT": "BTC/USD",
+            "ETHUSDT": "ETH/USD",
+            "MATICUSDT": "MATIC/USD",
+            "BTC/USD": "BTC/USD",
+            "ETH/USD": "ETH/USD",
+            "MATIC/USD": "MATIC/USD",
+            "market.btcusdt.trade.detail": "BTC/USD",
+            "market.maticusdt.trade.detail": "ETH/USD",
+            "market.ethusdt.trade.detail": "MATIC/USD"
         }
-        self.store = { i:{} for i in ["BTC/USD","ETH/USD","MATIC/USD"]}
+        self.store = {i: {} for i in ["BTC/USD", "ETH/USD", "MATIC/USD"]}       
         self.lock = Lock()
         self.len = 1
-        
-    def add(self,time,price,market):
-        self.lock.acquire()
-        if time in self.store[self.symbols[market]]:
-            self.store[self.symbols[market]][time].append(price)
-        else:
-            self.store[self.symbols[market]][time] = [price]
-            self.len+=1
-        self.lock.release()
-        
+
+    def __getitem__(self, market):
+        while self.lock.locked():
+            continue
+        with self.lock:
+            return self.store[market]
+
+    def add(self, time, price, market, client):
+        with self.lock:
+            if client == 'FTX':
+                time = int(datetime.strptime(
+                    time, "%Y-%m-%dT%H:%M:%S.%f%z").timestamp())
+            elif client == 'HUOBI' or client == 'BINANCE':
+                time = int(time/1000)
+
+            if client == 'BINANCE':
+                price = float(price)
+            # print(f"{client}   {int(datetime.now().timestamp())}  CURRENT")
+            print(
+                f"{client}   {time}   {self.symbols[market]}  {price} {type(price)}")
+            if time in self.store[self.symbols[market]]:
+                last_avg_price, num_prices = self.store[self.symbols[market]][time]
+                new_avg_price = ((last_avg_price*num_prices) +
+                                price)/(num_prices+1)
+                self.store[self.symbols[market]][time] = (
+                    new_avg_price, num_prices+1)
+            else:
+                self.store[self.symbols[market]][time] = (price, 1)
+                self.len += 1
+            # print(
+            #     f"{client} {time} {self.symbols[market]}  {self.store[self.symbols[market]][time]}")
+
     def __str__(self):
         return json.dumps(self.store)
+
     def __len__(self):
         return self.len
-    
