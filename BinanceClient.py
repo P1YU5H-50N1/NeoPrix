8aa2b218876e879950d095d8d2ac95bd0b8d57fe
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-08-23 12:34:51 +0530

    Formatted it a bit

--- BinanceClient.py
+++ BinanceClient.py
@@ -1,7 +1,10 @@
 import websocket
 from websocket import WebSocketApp
 from websocket._abnf import ABNF
+import sys
+from datetime import datetime 
 import json
+
 
 def on_message(ws, message):
     # print('BINANCE')
@@ -10,32 +13,33 @@ def on_message(ws, message):
         # print(message)
         return
     else:
-        time,market,price = message['E'],message['s'],message['p']
-        # ws.price.add(time,price,market)
-        print(f"BINANCE     {time} {market}  {price}")
-        
+        time, market, price = message['E'], message['s'], message['p']
+        ws.price.add(time, price, market,'BINANCE')
 
 def on_error(ws, error):
-    print(error)
+    print("BINANCE ERR", error)
+    sys.exit()
 
 def on_close(ws, close_status_code, close_msg):
     subscribe = {
         "method": "UNSUBSCRIBE",
         "params":
         [
-        "btcusdt@trade",
-        "ethusdt@trade",
-        "maticusdt@trade"
+            "btcusdt@trade",
+            "ethusdt@trade",
+            "maticusdt@trade"
         ],
         "id": 1
-        }
+    }
     payload = json.dumps(subscribe)
     ws.send(payload)
     print("### closed ###")
 
-def on_ping(ws,msg):
+
+def on_ping(ws, msg):
     print(msg)
-    ws.send(msg,ABNF.OPCODE_PONG)
+    ws.send(msg, ABNF.OPCODE_PONG)
+
 
 def on_open(ws):
     print("Opened connection")
@@ -43,32 +47,31 @@ def on_open(ws):
         "method": "SUBSCRIBE",
         "params":
         [
-        "btcusdt@aggTrade",
-        "ethusdt@aggTrade",
-        "maticusdt@aggTrade"
+            "btcusdt@aggTrade",
+            "ethusdt@aggTrade",
+            "maticusdt@aggTrade"
         ],
         "id": 1
-        }
+    }
     payload = json.dumps(subscribe)
     ws.send(payload)
 
-    
-   
-    
+
 class Binance(WebSocketApp):
-    
-    def __init__(self,rel,price):
+
+    def __init__(self, rel, price):
         self.price = price
         super().__init__("wss://stream.binance.com:9443/ws",
-                            on_open=on_open,
-                            on_ping = on_ping,
-                            on_message=on_message,
-                            on_error=on_error,
-                            on_close=on_close)
-        super().run_forever(ping_interval=10,dispatcher=rel)
+                         on_open=on_open,
+                         on_ping=on_ping,
+                         on_message=on_message,
+                         on_error=on_error,
+                         on_close=on_close)
+        super().run_forever(ping_interval=10, dispatcher=rel)
 
-def initializeBinance(rel,price):
-    client = Binance(rel,price)
+
+def initializeBinance(rel, price):
+    client = Binance(rel, price)
 
 
 if __name__ == "__main__":
