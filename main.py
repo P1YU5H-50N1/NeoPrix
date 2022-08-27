a5dba9a8afc0778a33d43ff8a0c82a7f2343dadf
P1YU5H-50N1 <me.piyush.360@gmail.com>
2022-08-27 21:26:57 +0530

    Refactored

--- main.py
+++ main.py
@@ -4,26 +4,11 @@ from FTXClient import initializeFTX
 from HuobiClient import initializeHuobi
 from Price_store import Price_store
 from Aggregator import Aggregator
+import websocket
 import rel
 import time
 import signal as sys_signal
 from threading import Event
-
-
-def clear_line(n=1):
-    LINE_UP = '\033[1A'
-    LINE_CLEAR = '\x1b[2K'
-    for i in range(n):
-        print(LINE_UP, end=LINE_CLEAR)
-
-
-def print_price(prices, evt):
-    while True:
-        if evt.is_set():
-            return
-        print(prices)
-    # clear_line(len(prices))
-
 
 evt = Event()
 agg = Aggregator(evt)
@@ -35,13 +20,11 @@ def handl():
     rel.abort()
     print('Exit')
 
-
+# websocket.enableTrace(True)
 with ThreadPoolExecutor() as executor:
     f1 = executor.submit(initializeBinance, rel, prices)
     f2 = executor.submit(initializeFTX, rel, prices)
     f3 = executor.submit(initializeHuobi, rel, prices)
-    f5 = executor.submit(agg.calculateAverage)
-    # f4 = executor.submit(print_price,prices,evt)
-    # print(f5.result())
+    f5 = executor.submit(agg.calculate)
     rel.signal(2, handl)  # Keyboard Interrupt
     rel.dispatch()
